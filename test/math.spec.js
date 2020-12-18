e22e3fb21c36b7f2ce06e8e29138debea3e38ec6
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2020-12-18 19:18:04 -0300

    installed chai

--- test/math.spec.js
+++ test/math.spec.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const Math = require('../src/math.js');
+const expect = require('chai').expect
 
 describe('Math class', function() {
     it('Sum two numbers', function(done) {
@@ -7,14 +8,27 @@ describe('Math class', function() {
         this.timeout(3000);
 
         math.sum(5, 5, (value) => {
-            assert.equal(value, 10);
+            expect(value).to.equal(10);
             done();
         });
     })
-    it.skip('Multiply two numbers', function() {
+    it('Multiply two numbers', function() {
         const math = new Math();
+        const obj = {
+            name: "Arnaldo"
+        }
+        const objClone = {
+            name: "Arnaldo"
+        }
 
-        asset.equal(math.multiply(5, 5), 25);
+        expect(obj).to.deep.equal(objClone);
+
+        //expect(obj).to.have.property('name').
+        //equal('Arnaldo');
+
+
+
+        //expect(math.multiply(5, 5)).to.equal(25);
 
     });
 });
